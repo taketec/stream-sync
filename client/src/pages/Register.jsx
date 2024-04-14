c0a1b160795369862f219eebefd62af7ae5caa02
taketec <shailupandit293@gmail.com>
2024-04-14 23:29:32 +0530

    cleaned up stuff

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,8 +6,7 @@ import { registerUser } from '../apis/auth'; // Assuming your register API funct
 const Register = () => {
   const navigate = useNavigate();
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
@@ -20,8 +19,7 @@ const Register = () => {
 
     try {
       const response = await registerUser({
-        firstName,
-        lastName,
+        username,
         email,
         password,
       });
@@ -39,33 +37,19 @@ const Register = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-3 rounded-lg bg-white ">
-        <h1 className="text-2xl font-bold text-center ">   streamsync 📺</h1>  {/* Larger font size and specific color */}
         <h1 className="text-2xl font-bold text-center">Register</h1>
         <p className='text-[#000000] text-[12px] tracking-wider font-medium'>Have Account ? <Link className='text-[rgba(0,195,154,1)] underline' to="/login">Sign in</Link></p>
         {errorMessage && <div className="text-red-500 text-center">{errorMessage}</div>}
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col">
-            <label htmlFor="firstName" className="text-sm font-medium text-gray-700">
-              First Name
-            </label>
-            <input
-              id="firstName"
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className="px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="flex flex-col">
             <label htmlFor="lastName" className="text-sm font-medium text-gray-700">
-              Last Name
+              Username
             </label>
             <input
               id="lastName"
               type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
               required
             />
@@ -106,7 +90,7 @@ const Register = () => {
             {isLoading ? 'Registering...' : 'Register'}
           </button>
         </form>
-        <h1 className="text-xs text-center ">welcome to streamsync 📺</h1>  {/* Larger font size and specific color */}
+        <h1 className="text-xs text-center ">welcome to depth-map-gen</h1>  {/* Larger font size and specific color */}
       </div>
     </div>
   );
