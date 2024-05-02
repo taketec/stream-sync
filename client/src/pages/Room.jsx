9be40ebd4e9161a722a5e401b6186c024025c908
taketec <shailupandit293@gmail.com>
2024-05-02 20:39:19 +0530

    feature added - video state is set on client if they join after its already changed on the server

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -8,7 +8,9 @@ import {get_global_time, median, timeout, get_jwt} from '../utils'
 
 let socket
 
-const THRESH_IGNORANCE = 0.1
+const THRESH_IGNORANCE = 0.2
+const THRESH_IGNORANCE_SEEK = 0.5
+
 const PLAYING_THRESH = 1 
 const PAUSED_THRESH = 0.01
 
@@ -17,14 +19,16 @@ const Room = () => {
   const [videoFilePath, setVideoFilePath] = useState(null);
   //const [timestamp, setTimestamp] = useState('');
   const playerRef = useRef(null);
-  const { roomId} = useParams();
+  const { roomId } = useParams();
   const [playPause, setPlayPause] = useState(false);
   //const state = useRef(null)
   const over_estimates = useRef([])
   const under_estimates = useRef([])
   const over_estimate = useRef(0)
-  const under_estimate = useRef(0)//i could have gotten away with just keeping the correction as a ref value and declaring other variables in the useeffect directly, but i like it this because of its simplicity
+  const under_estimate = useRef(0)//i could have gotten away with just keeping the correction as a ref value and declaring other variables in the useeffect directly, but i like it this because of its simplicity and for any future modifications
   const correction = useRef(0)
+
+  const is_playing  = useRef(false)
 
   let lastUpdated  = useRef(null)
   
@@ -51,58 +55,55 @@ const Room = () => {
         console.error('Error checking authentication:', error);
       }
     };
-    check_auth();
-  }, 
+    check_auth();}, 
   []); 
-  
+
+  const setVideoState = (state)=>{
+    console.log('got a new state update')
+    lastUpdated.current = get_global_time()
+    let proposed_time = (state.playing) ? ((state.video_timestamp - state.global_timestamp) + get_global_time(correction.current) ) : (state.video_timestamp)
+    let gap = Math.abs(proposed_time - playerRef.current.getCurrentTime())
+    
+    console.log(`%cGap was ${gap}`, 'font-size:12px; color:purple')
+    console.log(proposed_time)
+
+    is_playing.current = state.playing
+
+    if (state.playing){
+      if(gap > PLAYING_THRESH){
+        // tolerance while the video is playing
+        playerRef.current.seekTo(proposed_time, 'seconds')
+      }
+      
+      setPlayPause(state.playing)
+    }
+    else{
+      setPlayPause(state.playing)
+      if (gap > PAUSED_THRESH){
+        // condition to prevent an unnecessary seek
+        playerRef.current.seekTo(proposed_time, 'seconds')
+      
+      }
+    }
+  }
   useEffect(() => {
     const socket_listen = async() => {
       socket = io('http://localhost:8000')
       const response = await validUser();
-      let username = response.user.name;
+      let username
+      try{
+      username = response.user.name;
+      }
+      catch{
+        navigate('/login')
+      }
+      socket.emit('join_room', {room:roomId,username:username});
   
-      socket.emit('join room', {room:roomId,username:username});
-  
-      socket.on("paused",(playing)=>{
-        lastUpdated.current = get_global_time()
-        setPlayPause(playing)
-      })
-      socket.on('user-left-room' , (user) => {
+      socket.on('user_left_room' , (user) => {
         console.log(`user ${user} left room`)
       })
-      socket.on('seeked-to',(seconds)=>{
-        lastUpdated.current = get_global_time()
-        if(playerRef.current){playerRef.current.seekTo(seconds, 'seconds');}
-      })
 
-      socket.on('state_update_from_server',(state)=>{
-        console.log('got a new state update')
-        lastUpdated.current = get_global_time()
-        let proposed_time = (state.playing) ? ((state.video_timestamp - state.global_timestamp) + get_global_time(correction.current) ) : (state.video_timestamp)
-        let gap = Math.abs(proposed_time - playerRef.current.getCurrentTime())
-        
-        console.log(`%cGap was ${gap}`, 'font-size:12px; color:purple')
-        console.log(proposed_time)
-        if (state.playing){
-          if(gap > PLAYING_THRESH){
-            // tolerance while the video is playing
-            playerRef.current.seekTo(proposed_time, 'seconds')
-          }
-          setPlayPause(state.playing)
-        }
-        else{
-          setPlayPause(state.playing)
-          if (gap > PAUSED_THRESH){
-            // condition to prevent an unnecessary seek
-            playerRef.current.seekTo(proposed_time, 'seconds')
-          
-          }
-        }
-      
-        
-        
-
-      })
+      socket.on('state_update_from_server',setVideoState)
 
       socket.on("time_sync_response_backward", (time_at_server)=>{
         let under_estimate_latest =  time_at_server - get_global_time(0)
@@ -126,8 +127,7 @@ const Room = () => {
         socket.disconnect();
       };
     }
-    socket_listen()
-    },
+    socket_listen()},
    [])
   
   let do_time_sync_one_cycle_backward = () => {
@@ -161,30 +161,52 @@ const Room = () => {
 
 
   const handle_pause = () => {
+    is_playing.current = false
     if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE ){
-      socket.emit("paused",{room : roomId , playing:false})
+      let state_image = {
+        video_timestamp : playerRef.current.getCurrentTime(),
+        lastUpdated : get_global_time(correction.current),
+        playing:false,
+        global_timestamp: get_global_time(correction.current),
+        client_uid: get_jwt().substring(37,70)
+      }
+      console.log(`########### room-id ################ ${roomId}`)
+      socket.emit("state_update_from_client",{room : roomId ,state: state_image})
     }
     else{console.log(`ignored event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
   } 
   const handle_play = () => {
-    if (get_global_time() - lastUpdated.current > 0.2 ){
+    is_playing.current = true
+    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE ){
       let state_image = {
         video_timestamp : playerRef.current.getCurrentTime(),
         lastUpdated : get_global_time(correction.current),
         playing:true,
         global_timestamp: get_global_time(correction.current),
-        client_uid: get_jwt().substring(36,70)
+        client_uid: get_jwt().substring(37,70)
       }
       socket.emit("state_update_from_client",{room : roomId ,state: state_image})
     }else{console.log(`ignored play state update event event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
 
   } 
   const handle_seek = (seconds) => {
-    if (get_global_time() - lastUpdated.current > 0.5 ){
-      socket.emit("seeked-to",{room : roomId , timestamp: seconds})
+    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE_SEEK ){
+      let state_image = {
+        video_timestamp : seconds,//not using ref becuase its buggy, gives time before the seek event, the onSeek callback is built for this. 
+        lastUpdated : get_global_time(correction.current),
+        playing: is_playing.current,
+        global_timestamp: get_global_time(correction.current),
+        client_uid: get_jwt().substring(37,70)
+      }
+      socket.emit("state_update_from_client",{room : roomId ,state: state_image})
     }else{console.log(`ignored seek event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
   };
-
+  const onLoad = () =>{
+    if (get_global_time() - lastUpdated.current > 0.2 ){//need this condition for some buggy event firing, onReady gets called everytime when playing is changed 
+      socket.emit('explicit_state_request',roomId)
+    console.log('------------------------------- loaded-------------------------------------')
+    }
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen border rounded shadow">
@@ -205,6 +227,7 @@ const Room = () => {
             height="100%" 
             controls={true} 
             playing = {playPause}
+            onReady={onLoad}
           />
         </div>
       )}
--- server/index.js
+++ server/index.js
@@ -56,39 +56,36 @@ let rooms_state = {}
 let socket_user_map = {}
 io.on('connection', (socket) => {
 
-  socket.on('join room', ({room:room,username:username}) => {
-
+  socket.on('join_room', ({room:room,username:username}) => {
     socket_user_map[socket.id] = username//store username-socketid link 
 
     if (rooms_state[room] == undefined){
       console.log('new room created')
-      rooms_state[room] = {users:[],state:{playing:false}}; //adding default room state to a room if it doesnt already exist
-    }
-    
-    if (!rooms_state[room].users.includes(username)) { // check if the user is already in the room
-          rooms_state[room].users.push(username); // add the user to the rooms user list
-          //console.log(`User ${socket.id} joined room ${room}`);
-      } else {
-          console.log(`User ${socket.id} is already in room ${room}`);
+      let blank_state = {
+        video_timestamp : 0.0,
+        lastUpdated : get_time(),
+        playing:false,
+        global_timestamp: get_time(),
+        //client_uid: get_jwt().substring(37,70)
       }
-    console.log(`user ${username} joined ${room} `)
-    console.table(rooms_state)//all the code till now just manages the users state in the global room_state object, and after this the user joins he room
+
+      rooms_state[room] = { users:[], state:blank_state}; //adding default room state to a room if it doesnt already exist
+    }
+
+    if (rooms_state[room].users[socket.id] == undefined) { // check if the user is already in the room
+          rooms_state[room].users[socket.id] = username; // add the user to the rooms user list
+          //console.log(`User ${socket.id} joined room ${room}`);
+    } else {
+          console.log(`User ${socket.id} is already in room ${room}`);
+    }
+    console.log(`user ${rooms_state[room].users[socket.id]} joined ${room} `)
+    console.table(rooms_state[room])//all the code till now just manages the users state in the global room_state object, and after this the user joins he room
     socket.join(room);
   });
 
-  socket.on('paused', (data) => {
-    console.log("paused in ",data)
-    rooms_state[data.room].state.playing = data.playing 
-    console.table(rooms_state[data.room])
-    socket.to(data.room).emit('paused' , data.playing);
-  });
-
-  socket.on('seeked-to', (data) => {
-    console.log("seeked in ",data)
-    socket.to(data.room).emit("seeked-to" , data.timestamp);
-  });
-
-  
+  socket.on('explicit_state_request', (room) => {
+    socket.emit('state_update_from_server',rooms_state[room].state)
+  })
   
   socket.on('time_sync_request_backward', () => {
     socket.emit('time_sync_response_backward',get_time())
@@ -98,10 +95,11 @@ io.on('connection', (socket) => {
     socket.emit('time_sync_response_forward',get_time() - time_at_client)
   })
 
-  socket.on('state_update_from_client',({room:room,state:state}) =>{
-    rooms_state[room].state = state  
-    console.table(rooms_state[room])
-    socket.to(room).emit("state_update_from_server" , rooms_state[room].state);
+  socket.on('state_update_from_client',(data) =>{
+    console.log(data.state)
+    rooms_state[data.room].state = data.state  
+     console.table(rooms_state[data.room])
+    socket.to(data.room).emit("state_update_from_server" , rooms_state[data.room].state);
   })
 
   //have to use disconnecting here because once the socket is 'disconnected' its room data is lost
@@ -112,15 +110,16 @@ io.on('connection', (socket) => {
     //e[0] socketid if the current client 
     //e[1] room the socket is in
 
-    console.log(socket_user_map[e[0]])
+    console.log(e)
 
-    socket.to(e[1]).emit('user-left-room' , socket_user_map[e[0]])
+    //socket.to(e[1]).emit('user_left_room' , rooms_state[e[1]].users[e[0]])
+    delete rooms_state[e[1]].users[e[0]]; 
     //logic to remove the user from the room 
-    console.table(rooms_state)
+    console.table(rooms_state[e[1]])
   };
     }
   catch(error){
-    console.log(e, error)
+    console.log(error)
   }}
 )
 
