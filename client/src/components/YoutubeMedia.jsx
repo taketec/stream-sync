80a2117d1e02553b80f0cbb6c6eddd8d2f79ef8f
taketec <shailupandit293@gmail.com>
2024-05-05 18:04:05 +0530

    decoupled youtube player from room component

---
+++ client/src/components/YoutubeMedia.jsx
@@ -0,0 +1,214 @@
+
+import React, { useState, useRef, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+import { useNavigate } from 'react-router-dom';
+import {get_global_time, get_jwt} from '../utils'
+
+const THRESH_IGNORANCE = 0.2
+const THRESH_IGNORANCE_SEEK = 0.5
+
+const PLAYING_THRESH = 1 
+const PAUSED_THRESH = 0.01
+
+
+
+const YouTubeMedia = (props) => {
+  let socket
+  let roomId = props.room
+  let correction = props.correction
+  const [inputValue, setInputValue] = useState('');
+  const [videoUrl, setVideoUrl] = useState('https://www.youtube.com/watch?v=gU-8U7Z-E64');
+
+  const playerRef = useRef(null);
+
+  const [playPause, setPlayPause] = useState(false);
+
+  let lastUpdated  = useRef(null)
+
+  const is_playing  = useRef(false)
+
+  const setVideoState = (state)=>{
+    if (playerRef.current){
+    
+    if (state.url !== videoUrl){
+      setVideoUrl(state.url)
+
+    } 
+    
+    lastUpdated.current = get_global_time()
+    let proposed_time = (state.playing) ? ((state.video_timestamp - state.global_timestamp) + get_global_time(correction) ) : (state.video_timestamp)
+    let gap = Math.abs(proposed_time - playerRef.current.getCurrentTime())
+    
+    console.log(`%cGap was ${gap}`, 'font-size:12px; color:purple')
+    console.log(proposed_time)
+
+    is_playing.current = state.playing
+    console.log(`#############################playing set to ${state.playing} `)
+    if (state.playing){
+      if(gap > PLAYING_THRESH){
+        // tolerance while the video is playing
+        playerRef.current.seekTo(proposed_time, 'seconds')
+      }
+      setPlayPause(state.playing)
+    }
+    else{
+      setPlayPause(state.playing)
+      if (gap > PAUSED_THRESH){
+        // condition to prevent an unnecessary seek
+        playerRef.current.seekTo(proposed_time, 'seconds')
+      
+      }
+    }}
+  }
+
+  useEffect(()=>{
+    const socket_listen = () => {
+        socket = props.socket
+        if (socket){
+        socket.on('state_update_from_server',setVideoState)
+        return () => {
+          socket.off('state_update_from_server');
+        };
+      }else{console.log('socket was null')}}
+      socket_listen()
+    },
+    []
+  )
+
+  useEffect(() => {console.log('playing set to -> ',playPause)},[playPause])//logs playing/pausing state
+
+  const handleVideoChange = (e) => {
+    e.preventDefault();
+    setVideoUrl(inputValue);
+    is_playing.current = false
+    let state_image = {
+      media : 'youtube',
+      url : inputValue,
+      video_timestamp : 0,//removing the delay 
+      lastUpdated : get_global_time(correction),
+      playing:false,
+      global_timestamp: get_global_time(correction),
+      client_uid: get_jwt().substring(37,70)
+    }
+    props.socket.emit("state_update_from_client",{room : roomId ,state: state_image})
+
+
+    setInputValue('');
+  };
+
+    
+
+  const handle_pause = () => {
+    is_playing.current = false
+    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE ){
+      let state_image = {
+        media : 'youtube',
+        url : videoUrl,  
+        video_timestamp : playerRef.current.getCurrentTime(),
+        lastUpdated : get_global_time(correction),
+        playing:false,
+        global_timestamp: get_global_time(correction),
+        client_uid: get_jwt().substring(37,70)
+      }
+      //console.log(`########### room-id ################ ${roomId}`)
+      props.socket.emit("state_update_from_client",{room : roomId ,state: state_image})
+    }
+    else{console.log(`ignored event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
+  } 
+  const handle_play = () => {
+    is_playing.current = true
+    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE ){
+      let state_image = {
+        media : 'youtube',
+        url : videoUrl,  
+        video_timestamp : playerRef.current.getCurrentTime(),
+        lastUpdated : get_global_time(correction),
+        playing:true,
+        global_timestamp: get_global_time(correction),
+        client_uid: get_jwt().substring(37,70)
+      }
+      props.socket.emit("state_update_from_client",{room : roomId ,state: state_image})
+    }else{console.log(`ignored play state update event event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
+
+  } 
+  const handle_seek = async(seconds) => {
+    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE_SEEK ){
+      let timestamp;
+      console.log('on seek called')
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          timestamp = playerRef.current.getCurrentTime();
+          resolve();
+        }, 50); // gives time before the seek event, thus the delay is needed
+      });
+      
+      let state_image = {
+        media : 'youtube',
+        url : videoUrl,  
+        video_timestamp : seconds-0.05,//removing the delay 
+        lastUpdated : get_global_time(correction),
+        playing:is_playing.current,
+        global_timestamp: get_global_time(correction),
+        client_uid: get_jwt().substring(37,70)
+      }
+      props.socket.emit("state_update_from_client",{room : roomId ,state: state_image})
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          console.log('this is the post seek update')
+          props.socket.emit('explicit_state_request',roomId)
+          resolve();
+        }, 4000); // gives time before the seek event, thus the delay is needed
+      });
+
+    }else{console.log(`ignored seek event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
+  };
+  
+  const onLoad = () =>{
+    if (get_global_time() - lastUpdated.current > 0.2 ){//need this condition for some buggy event firing, onReady gets called everytime when playing is changed 
+      props.socket.emit('explicit_state_request',roomId)
+      console.log('------------------------------- loaded-------------------------------------')
+    }
+  }
+
+
+    return(
+      <div className="flex flex-col items-center w-full"> {/* Parent div */}
+        <div className="relative" style={{ paddingTop: '1%', width: '100%' }}>
+          <form onSubmit={handleVideoChange}>
+            <div className="m-0">
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Enter only valid url"
+              />
+            </div>
+            <button 
+              type="submit"
+              className={`my-2 px-2 py-1 rounded-lg bg-blue-500 text-white text-sm font-medium hover:bg-blue-700`}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        
+        <div className="relative" style={{ paddingTop: '0%', width: '100%' }}>
+          <ReactPlayer 
+            ref={playerRef}
+            onSeek={handle_seek}//SEEK EVENT GETTER
+            onPause={handle_pause}
+            onPlay={handle_play}
+            onBufferEnd={handle_play}
+            url={videoUrl}
+            className="absolute top-0 left-0 w-full h-full"
+            playing={playPause}
+            controls={true} 
+            onReady={onLoad}
+          />
+        </div>
+      </div>
+
+      )
+    }
+
+export default YouTubeMedia
--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,34 +1,25 @@
 import React, { useState, useRef, useEffect } from 'react';
-import ReactPlayer from 'react-player';
 import { useParams } from 'react-router-dom';
 import io from "socket.io-client"
 import { useNavigate } from 'react-router-dom';
 import { validUser } from '../apis/auth';
-import {get_global_time, median, timeout, get_jwt} from '../utils'
+import {get_global_time, median, timeout} from '../utils'
 import Userlist from '../components/UsersList';
+import YoutubeMedia from '../components/YoutubeMedia';
 
 let socket
 
-const THRESH_IGNORANCE = 0.2
-const THRESH_IGNORANCE_SEEK = 0.5
-
-const PLAYING_THRESH = 1 
-const PAUSED_THRESH = 0.01
 
 //let TEMP_URL = `https://rr2---sn-i5uif5t-2o9l.googlevideo.com/videoplayback?expire=1714277406&ei=vnctZsyrD8Dlz7sPvvqgMA&ip=116.75.30.244&id=o-AJfpBMoiQhwvIrm0adn0w17ZGIWC7IO2af74g-_2LEDY&itag=22&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=CG&mm=31%2C29&mn=sn-i5uif5t-2o9l%2Csn-gwpa-qxa6&ms=au%2Crdu&mv=m&mvi=2&pl=22&gcr=in&initcwndbps=1453750&bui=AWRWj2RtyyTRuxLiITjVeDbfpbuM2n8_rz9p2W5krbbSmVNb8JO9IUMk_STh8ce5Hi7i_h_U202QT4kA&spc=UWF9f9tRwoXxBei2OPalfjRMKeinzk4SgI7COEcHwnWby-ZhVmRd3JXiXQui&vprv=1&svpuc=1&mime=video%2Fmp4&ns=494aXBfI6v6IaDlKgzNkMHUQ&cnr=14&ratebypass=yes&dur=164.606&lmt=1661553915691651&mt=1714255501&fvip=7&c=WEB&sefc=1&txp=6318224&n=Y0jPXFmaEyhch9F3lq&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHWaYeowRQIgdzqSdW-fiGQuj_LH7QKwRYraH2sHTllYQtmzISx96UMCIQDmVb8srTDfaJlS_i5pP8fjnbu3YUZygFDA0QCxtLzcvg%3D%3D&sig=AJfQdSswRAIgQHgXQe-qPgqVG0d1ROZ4Rn40hYKCMZt0tAYxmCh87h4CIDTc5BkddNQWEnXo_A2rBzQZ_PiRQJJgB4DhWceZFGi7`
 const Room = () => {
 
-  const [inputValue, setInputValue] = useState('');
-  const [videoUrl, setVideoUrl] = useState('https://www.youtube.com/watch?v=gU-8U7Z-E64');
   
   const [currentSocket,setSocket] = useState(null)
 
   const [users, setUsers] = useState([]);
 
   //const [timestamp, setTimestamp] = useState('');
-  const playerRef = useRef(null);
   const { roomId } = useParams();
-  const [playPause, setPlayPause] = useState(false);
   //const state = useRef(null)
   const over_estimates = useRef([])
   const under_estimates = useRef([])
@@ -36,9 +27,7 @@ const Room = () => {
   const under_estimate = useRef(0)//i could have gotten away with just keeping the correction as a ref value and declaring other variables in the useeffect directly, but i like it this because of its simplicity and for any future modifications
   const [correction , setCorrection] = useState(0.0)
 
-  const is_playing  = useRef(false)
 
-  let lastUpdated  = useRef(null)
   
   const navigate = useNavigate();
 
@@ -66,44 +55,10 @@ const Room = () => {
     check_auth();}, 
   []); 
 
-  const setVideoState = (state)=>{
-    if (playerRef.current){
-    
-    if (state.url !== videoUrl){
-      setVideoUrl(state.url)
-
-    } 
-    
-    lastUpdated.current = get_global_time()
-    let proposed_time = (state.playing) ? ((state.video_timestamp - state.global_timestamp) + get_global_time(correction) ) : (state.video_timestamp)
-    let gap = Math.abs(proposed_time - playerRef.current.getCurrentTime())
-    
-    console.log(`%cGap was ${gap}`, 'font-size:12px; color:purple')
-    console.log(proposed_time)
-
-    is_playing.current = state.playing
-    console.log(`#############################playing set to ${state.playing} `)
-    if (state.playing){
-      if(gap > PLAYING_THRESH){
-        // tolerance while the video is playing
-        playerRef.current.seekTo(proposed_time, 'seconds')
-      }
-      setPlayPause(state.playing)
-    }
-    else{
-      setPlayPause(state.playing)
-      if (gap > PAUSED_THRESH){
-        // condition to prevent an unnecessary seek
-        playerRef.current.seekTo(proposed_time, 'seconds')
-      
-      }
-    }}
-  }
 
   useEffect(() => {
     const socket_listen = async() => {
       socket = io('http://192.168.1.4:8000')
-      setSocket(socket)
       const response = await validUser();
       let username
       try{
@@ -117,8 +72,6 @@ const Room = () => {
       socket.on('user_left_room' , (user) => {
         console.log(`user ${user} left room`)
       })
-
-      socket.on('state_update_from_server',setVideoState)
 
       socket.on("time_sync_response_backward", (time_at_server)=>{
         let under_estimate_latest =  time_at_server - get_global_time(0)
@@ -141,6 +94,7 @@ const Room = () => {
       socket.on('userlist_update',(userlist)=>{
         setUsers(userlist)
         console.table(userlist)
+        setSocket(socket)
       })
       return () => {
         socket.disconnect();
@@ -171,139 +125,16 @@ const Room = () => {
   },[])
 
   useEffect(() => console.log(roomId),[roomId])//logs room id
-  useEffect(() => {console.log('playing set to -> ',playPause)},[playPause])//logs playing/pausing state
-
-  const handleVideoChange = (e) => {
-    e.preventDefault();
-    setVideoUrl(inputValue);
-    is_playing.current = false
-    let state_image = {
-      media : 'youtube',
-      url : inputValue,
-      video_timestamp : 0,//removing the delay 
-      lastUpdated : get_global_time(correction),
-      playing:false,
-      global_timestamp: get_global_time(correction),
-      client_uid: get_jwt().substring(37,70)
-    }
-    socket.emit("state_update_from_client",{room : roomId ,state: state_image})
-
-
-    setInputValue('');
-  };
 
 
 
-  const handle_pause = () => {
-    is_playing.current = false
-    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE ){
-      let state_image = {
-        media : 'youtube',
-        url : videoUrl,  
-        video_timestamp : playerRef.current.getCurrentTime(),
-        lastUpdated : get_global_time(correction),
-        playing:false,
-        global_timestamp: get_global_time(correction),
-        client_uid: get_jwt().substring(37,70)
-      }
-      //console.log(`########### room-id ################ ${roomId}`)
-      socket.emit("state_update_from_client",{room : roomId ,state: state_image})
-    }
-    else{console.log(`ignored event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
-  } 
-  const handle_play = () => {
-    is_playing.current = true
-    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE ){
-      let state_image = {
-        media : 'youtube',
-        url : videoUrl,  
-        video_timestamp : playerRef.current.getCurrentTime(),
-        lastUpdated : get_global_time(correction),
-        playing:true,
-        global_timestamp: get_global_time(correction),
-        client_uid: get_jwt().substring(37,70)
-      }
-      socket.emit("state_update_from_client",{room : roomId ,state: state_image})
-    }else{console.log(`ignored play state update event event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
-
-  } 
-  const handle_seek = async(seconds) => {
-    if (get_global_time() - lastUpdated.current > THRESH_IGNORANCE_SEEK ){
-      let timestamp;
-      console.log('on seek called')
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          timestamp = playerRef.current.getCurrentTime();
-          resolve();
-        }, 50); // gives time before the seek event, thus the delay is needed
-      });
-      
-      let state_image = {
-        media : 'youtube',
-        url : videoUrl,  
-        video_timestamp : seconds-0.05,//removing the delay 
-        lastUpdated : get_global_time(correction),
-        playing:is_playing.current,
-        global_timestamp: get_global_time(correction),
-        client_uid: get_jwt().substring(37,70)
-      }
-      socket.emit("state_update_from_client",{room : roomId ,state: state_image})
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          console.log('this is the post seek update')
-          socket.emit('explicit_state_request',roomId)
-          resolve();
-        }, 4000); // gives time before the seek event, thus the delay is needed
-      });
-
-    }else{console.log(`ignored seek event due to it being a result of unwanted event being fired, last updated = ${lastUpdated.current}`)}
-  };
-  
-  const onLoad = () =>{
-    if (get_global_time() - lastUpdated.current > 0.2 ){//need this condition for some buggy event firing, onReady gets called everytime when playing is changed 
-      socket.emit('explicit_state_request',roomId)
-    console.log('------------------------------- loaded-------------------------------------')
-    }
-  }
 
   return (
       <div className="flex h-screen border rounded shadow">
         <div className="flex-1 flex flex-row">
           <div className="p-4 flex-1">
-          <div className="my-4"> 
-          <form onSubmit={handleVideoChange}>
-            <div className="m-0">
-              <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Enter only valid url"
-              />
-            </div>
-            <button 
-            type="submit"
-            className={`my-2 px-2 py-1 rounded-lg bg-blue-500 text-white text-sm font-medium hover:bg-blue-700`}>
-              Submit
-            </button>
-          </form>
           </div>
-            {(
-              <div className="relative" style={{ paddingTop: '0%', width: '100%' }}>
-                <ReactPlayer 
-                  ref={playerRef}
-                  onSeek={handle_seek}//SEEK EVENT GETTER
-                  onPause={handle_pause}
-                  onPlay={handle_play}
-                  onBufferEnd={handle_play}
-                  url={videoUrl}
-                  className="absolute top-0 left-0 w-full h-full"
-                  playing={playPause}
-                  controls={true} 
-                  onReady={onLoad}
-                />
-              </div>
-            )}
-          </div>
+          {socket && <YoutubeMedia socket = {currentSocket} room = {roomId} correction = {correction}/>}
           <div className="overflow-y-auto pt-5" style={{ flex: '0 0 auto' }}>
             <Userlist user_list={users}/>
           </div>
