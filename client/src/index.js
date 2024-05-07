85b923fb94141b4fc6ceea8e68466b1843dba161
taketec <shailupandit293@gmail.com>
2024-05-07 17:28:08 +0530

    cleaned up some code

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId="26649422327-mb8qu858s8l7f4fp01pdkiv499ppe997.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId = {process.env.clientId}>
     <App />
   </GoogleOAuthProvider>,
 );
