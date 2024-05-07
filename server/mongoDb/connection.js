d58643b173b8f2e4124ea7e2ab5e03a13ffa9c40
taketec <shailupandit293@gmail.com>
2024-05-07 21:08:42 +0530

    added vercel.json

--- server/mongoDb/connection.js
+++ server/mongoDb/connection.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const mongoDBConnect = () => {
   try {
-    mongoose.connect(process.env.URL, {
+    mongoose.connect(process.env.PROD_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    console.log("MongoDB - Connected at " + process.env.URL);
+    console.log("MongoDB - Connected at " + process.env.PROD_URL);
   } catch (error) {
     console.log("Error - MongoDB Connection " + error);
   }
