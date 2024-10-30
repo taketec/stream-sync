865b6d22bf292899a3c300027e0cfdac908d31ed
taketec <shailupandit293@gmail.com>
2024-10-30 11:30:19 +0530

    installed serve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login'; // Import your Login component
 //import Register from './pages/Register';
 import Room from './pages/Room';
 
 function App() {
-  useEffect(()=>{console.log("token",import.meta.env.VITE_REACT_APP_clientId)},[])
   return (
     <Router>
       <Routes>
