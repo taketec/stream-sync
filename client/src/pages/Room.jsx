f2e387c82cfb602e777acaed5ec19bf095b0ea6d
shai <shailupandit293@gmail.com>
2024-11-03 18:53:40 +0530

    Update Room.jsx

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -197,7 +197,7 @@ const Room = () => {
             )}
           </div>
         </div>
-        <div className="overflow-y-auto pt-5 bg-gray-100 dark:bg-gray-900 dark:text-gray-100" style={{ flex: '0 0 auto' }}>
+        <div className="overflow-y-auto pt-5 bg-gray-100 dark:bg-gray-800 dark:text-gray-100" style={{ flex: '0 0 auto' }}>
           <Userlist user_list={users} />
         </div>
       </div>
