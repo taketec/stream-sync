c7cda913c87db8d72c8ce32a218e80110882198e
SohamWandkar <sohamwandkar3114@gmail.com>
2024-11-13 00:05:15 +0530

    adding to amplify

--- client/src/apis/auth.js
+++ client/src/apis/auth.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//export const url = process.env.URL'https://stream-sync-production-6ee8.up.railway.app';
-//export const url = import.meta.env.VITE_BACKEND_URL.trim() || 'http://localhost:8000';
-export const url = 'http://localhost:8000';
+//export const url = 'http://ec2-13-233-48-225.ap-south-1.compute.amazonaws.com:8000';
+export const url = import.meta.env.VITE_BACKEND_URL.trim() || 'https://stream-sync-production-6ee8.up.railway.app';
+//export const url = 'http://localhost:8000';
 
 console.log(localStorage.getItem('userToken'))
 
